5179e6a30a0514dc9dc3cda9484196204c90daa3
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2025-04-30 16:09:31 +0100

    check your code works before you commit it...

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,4 @@
-const url = "https://di-week-08-demo.onrender.com/";
+const url = "https://di-week-08-demo.onrender.com";
 
 document.getElementById("button").addEventListener("click", async () => {
   document.getElementById("mylist").innerHTML = "<p>Loading......</p>";
